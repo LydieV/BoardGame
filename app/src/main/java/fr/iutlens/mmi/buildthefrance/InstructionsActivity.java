2258341c62468c9007523280257a7f94a4d60625
lydie.vittu <lydie_vittu@ens.univ-artois.fr>
2020-03-04 13:54:00 +0100

    Ajout bouton retour instructions + modif taille pour page perdu et accueil.

--- app/src/main/java/fr/iutlens/mmi/buildthefrance/InstructionsActivity.java
+++ app/src/main/java/fr/iutlens/mmi/buildthefrance/InstructionsActivity.java
@@ -1,7 +1,10 @@
 package fr.iutlens.mmi.buildthefrance;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class InstructionsActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class InstructionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
+
+        View view = findViewById(R.id.buttonRetour);
+        view.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                retour(view);
+            }
+        });
+    }
+
+    private void retour(View view){
+        Button button = (Button) findViewById(R.id.buttonRetour);
+        Intent intent = new Intent(this, AccueilActivity.class);
+        startActivity(intent);
     }
 }
